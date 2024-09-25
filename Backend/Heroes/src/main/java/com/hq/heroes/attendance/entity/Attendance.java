6e880c1b3acc2b8f2d9ef21d76bbec0bf8bcc815
tyeong1102 <tyeong1102@gmail.com>
2024-09-25 15:30:20 +0900

    feat : attendance 엔티티 및 enum 추가

--- Backend/Heroes/src/main/java/com/hq/heroes/attendance/entity/Attendance.java
+++ Backend/Heroes/src/main/java/com/hq/heroes/attendance/entity/Attendance.java
@@ -1,4 +1,45 @@
 package com.hq.heroes.attendance.entity;
 
+import com.hq.heroes.attendance.entity.enums.AttendanceStatus;
+import com.hq.heroes.auth.entity.Employee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "tb_attendance")
 public class Attendance {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "attendance_id", nullable = false)
+    private Long attendanceId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "check_in", nullable = false)
+    private LocalDateTime checkIn;
+
+    @Column(name = "check_out", nullable = false)
+    private LocalDateTime checkOut;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private AttendanceStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
