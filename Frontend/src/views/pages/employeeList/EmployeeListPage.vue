ec8ecb10bf66ff8b8b2561a89e9f9c2c7e2ce760
mmvne <kingtjrals@naver.com>
2024-09-25 09:28:36 +0900

    design : employeeList 칭찬기능 삭제 및 상태 추가

--- Frontend/src/views/pages/employeeList/EmployeeListPage.vue
+++ Frontend/src/views/pages/employeeList/EmployeeListPage.vue
@@ -5,34 +5,25 @@
             :value="filteredEmployees"
             :paginator="true"
             :rows="10"
+            removableSort
             dataKey="employeeNo"
             :rowHover="true"
             v-model:filters="filters"
             filterDisplay="menu"
             :filters="filters"
+            selectionMode="single"
             :globalFilterFields="['employeeName', 'department', 'address', 'email']"
             showGridlines
             @row-click="showEmployeeDetails"
+            :metaKeySelection="false"
+            @rowSelect="onRowSelect"
+            @rowUnselect="onRowUnselect"
         >
             <template #header>
                 <div class="flex justify-between items-center">
                     <div class="flex items-center gap-2">
-                        <Dropdown
-                            v-model="selectedDepartment"
-                            :options="departments"
-                            optionLabel="name"
-                            placeholder="부서를 선택하세요"
-                            @change="filterByDepartmentAndTeam"
-                            class="mr-2"
-                        />
-                        <Dropdown
-                            v-model="selectedTeam"
-                            :options="teams"
-                            optionLabel="name"
-                            placeholder="팀을 선택하세요"
-                            @change="filterByDepartmentAndTeam"
-                            class="mr-2"
-                        />
+                        <Dropdown v-model="selectedDepartment" :options="departments" optionLabel="name" placeholder="부서를 선택하세요" @change="filterByDepartmentAndTeam" class="mr-2" />
+                        <Dropdown v-model="selectedTeam" :options="teams" optionLabel="name" placeholder="팀을 선택하세요" @change="filterByDepartmentAndTeam" class="mr-2" />
                     </div>
                     <div class="relative search-container">
                         <i class="pi pi-search search-icon" />
@@ -41,43 +32,44 @@
                 </div>
             </template>
             <template #empty> No employees found. </template>
-            <Column field="department" header="부 서" style="min-width: 12rem">
+            <Column field="department" sortable header="부 서" style="min-width: 12rem">
                 <template #body="{ data }">
                     {{ data.department }}
                 </template>
             </Column>
-            <Column field="teamName" header="팀" style="min-width: 12rem">
+            <Column field="teamName" sortable header="팀" style="min-width: 12rem">
                 <template #body="{ data }">
                     {{ data.teamName }}
                 </template>
             </Column>
-            <Column field="employeeName" header="이 름" style="min-width: 12rem">
+            <Column field="employeeName" sortable header="이 름" style="min-width: 12rem">
                 <template #body="{ data }">
                     {{ data.employeeName }}
                 </template>
             </Column>
-            <Column field="position" header="직 책" style="min-width: 12rem">
+            <Column field="position" sortable header="직 책" style="min-width: 12rem">
                 <template #body="{ data }">
                     {{ data.position }}
                 </template>
             </Column>
-            <Column field="employeeId" header="사 번" style="min-width: 12rem">
+            <Column field="employeeId" sortable header="사 번" style="min-width: 12rem">
                 <template #body="{ data }">
                     {{ data.employeeId }}
                 </template>
             </Column>
-            <Column field="hireDate" header="입사일" dataType="date" style="min-width: 10rem">
+            <Column field="hireDate" sortable header="입사일" dataType="date" style="min-width: 10rem">
                 <template #body="{ data }">
                     {{ formatDate(new Date(data.hireDate)) }}
                 </template>
             </Column>
+            <Column field="status" sortable header="상 태" style="min-width: 10rem">
+                <template #body="{ data }">
+                    {{ data.status }}
+                </template>
+            </Column>
         </DataTable>
 
-        <EmployeeDetailModal 
-            :employee="selectedEmployee" 
-            :visible="displayDialog" 
-            @update:visible="displayDialog = $event" 
-        />
+        <EmployeeDetailModal :employee="selectedEmployee" :visible="displayDialog" @update:visible="displayDialog = $event" />
     </div>
 </template>
 
@@ -105,10 +97,11 @@ const selectedTeam = ref(null);
 // API 호출하여 직원 데이터 가져오기
 async function fetchEmployeeList() {
     employees.value = [
-        { employeeNo: 1, employeeName: '홍길동', department: '개발', teamName: '팀A', position: '개발자', employeeId: 'E001', hireDate: '2022-01-01', praiseCount: 0 },
-        { employeeNo: 2, employeeName: '이순신', department: '개발', teamName: '팀B', position: '팀장', employeeId: 'E002', hireDate: '2021-02-01', praiseCount: 0 },
-        { employeeNo: 3, employeeName: '김유신', department: '디자인', teamName: '팀C', position: '디자이너', employeeId: 'E003', hireDate: '2020-03-01', praiseCount: 0 }
+        { employeeNo: 1, employeeName: '홍길동', department: '개발', teamName: '팀A', position: '개발자', employeeId: 'E001', hireDate: '2022-01-01', status: '근무중' },
+        { employeeNo: 2, employeeName: '이순신', department: '개발', teamName: '팀B', position: '팀장', employeeId: 'E002', hireDate: '2021-02-01', status: '퇴근' },
+        { employeeNo: 3, employeeName: '김유신', department: '디자인', teamName: '팀C', position: '디자이너', employeeId: 'E003', hireDate: '2020-03-01', status: '휴가' }
     ];
+
 
     filterByDepartmentAndTeam(); // 필터링 함수 호출
 }
@@ -133,12 +126,8 @@ async function fetchTeams() {
 
 function filterByDepartmentAndTeam() {
     filteredEmployees.value = employees.value.filter((employee) => {
-        const matchesDepartment = selectedDepartment.value && selectedDepartment.value.name !== '전체'
-            ? employee.department === selectedDepartment.value.name
-            : true;
-        const matchesTeam = selectedTeam.value && selectedTeam.value.name !== '전체'
-            ? employee.teamName === selectedTeam.value.name
-            : true;
+        const matchesDepartment = selectedDepartment.value && selectedDepartment.value.name !== '전체' ? employee.department === selectedDepartment.value.name : true;
+        const matchesTeam = selectedTeam.value && selectedTeam.value.name !== '전체' ? employee.teamName === selectedTeam.value.name : true;
         return matchesDepartment && matchesTeam;
     });
 }
@@ -148,7 +137,7 @@ function initFilters() {
         global: { value: null, matchMode: FilterMatchMode.CONTAINS },
         employeeName: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }] },
         department: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }] },
-        hireDate: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] },
+        hireDate: { operator: FilterOperator.AND, constraints: [{ value: null, matchMode: FilterMatchMode.DATE_IS }] }
     };
 }
 
