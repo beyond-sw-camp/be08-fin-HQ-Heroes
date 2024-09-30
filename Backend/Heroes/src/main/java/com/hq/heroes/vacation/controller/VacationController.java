f87f3e602121c293fd446c19338c3f7d474891f4
tyeong1102 <tyeong1102@gmail.com>
2024-09-30 14:40:18 +0900

    feat : 휴가 신청, 휴가 조회 기능 추가

--- Backend/Heroes/src/main/java/com/hq/heroes/vacation/controller/VacationController.java
+++ Backend/Heroes/src/main/java/com/hq/heroes/vacation/controller/VacationController.java
@@ -1,6 +1,10 @@
 package com.hq.heroes.vacation.controller;
 
+import com.hq.heroes.vacation.dto.VacationDTO;
+import com.hq.heroes.vacation.service.VacationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -10,4 +14,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class VacationController {
 
+    private final VacationService vacationService;
+
+    @PostMapping("/submit")
+    @Operation(summary = "휴가 신청")
+    public ResponseEntity<String> submitVacation(@RequestBody VacationDTO vacationDTO) {
+        try {
+            vacationService.submitVacation(vacationDTO);
+            return ResponseEntity.ok("휴가 신청이 성공적으로 제출되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("휴가 신청 중 오류가 발생했습니다: " + e.getMessage());
+        }
+    }
+
+    @GetMapping("/list")
+    @Operation(summary = "휴가 신청 목록 조회")
+    public ResponseEntity<List<VacationDTO>> getVacationList() {
+        List<VacationDTO> lists = vacationService.getAllVacations();
+        return ResponseEntity.ok(lists);
+    }
 }
