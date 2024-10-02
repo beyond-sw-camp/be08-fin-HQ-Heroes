1bbc4b41f9fe970f2589e7daadacb567e3c350cd
jongwon <growjong8802@gmail.com>
2024-10-02 15:17:34 +0900

    feat : 평가 기준 및 평가 기록 조회, 등록, 수정, 삭제 기능 추가

--- Backend/Heroes/src/main/java/com/hq/heroes/evaluation/dto/EvaluationResDTO.java
+++ Backend/Heroes/src/main/java/com/hq/heroes/evaluation/dto/EvaluationResDTO.java
@@ -14,8 +14,8 @@ import java.util.Date;
 public class EvaluationResDTO {
 
     private Long evaluationId;  // 평가 ID
-    private String employeeName;  // 피평가자 이름
-    private String evaluatorName;  // 평가자 이름
+    private String employeeId;  // 피평가자 ID
+    private String evaluatorId;  // 평가자 ID
     private String evaluationTemplateTitle;  // 평가 기준 제목
     private Double score;  // 평가 점수
     private String comments;  // 평가 코멘트
--- Backend/Heroes/src/main/java/com/hq/heroes/evaluation/service/EvaluationServiceImpl.java
+++ Backend/Heroes/src/main/java/com/hq/heroes/evaluation/service/EvaluationServiceImpl.java
@@ -1,9 +1,14 @@
 package com.hq.heroes.evaluation.service;
 
 import com.hq.heroes.auth.entity.Employee;
+import com.hq.heroes.employee.entity.Department;
+import com.hq.heroes.employee.repository.DepartmentRepository;
+import com.hq.heroes.evaluation.dto.EvaluationCriteriaReqDTO;
 import com.hq.heroes.evaluation.dto.EvaluationReqDTO;
 import com.hq.heroes.evaluation.entity.Evaluation;
 import com.hq.heroes.auth.repository.EmployeeRepository;
+import com.hq.heroes.evaluation.entity.EvaluationCriteria;
+import com.hq.heroes.evaluation.repository.EvaluationCriteriaRepository;
 import com.hq.heroes.evaluation.repository.EvaluationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +23,8 @@ public class EvaluationServiceImpl implements EvaluationService {
 
     private final EvaluationRepository evaluationRepository;
     private final EmployeeRepository employeeRepository;
+    private final EvaluationCriteriaRepository evaluationCriteriaRepository;
+    private final DepartmentRepository departmentRepository;
 
     @Override
     public List<Evaluation> getEvaluations() {
@@ -34,9 +41,12 @@ public class EvaluationServiceImpl implements EvaluationService {
     @Transactional
     public Evaluation createEvaluation(EvaluationReqDTO requestDTO) {
 
+        System.out.println(requestDTO.toString());
+
         // 평가자와 피평가자 정보 가져옴
-        Optional<Employee> employeeOpt = employeeRepository.findById(String.valueOf(requestDTO.getEmployeeId()));
-        Optional<Employee> evaluatorOpt = employeeRepository.findById(String.valueOf(requestDTO.getEvaluatorId()));
+        Optional<Employee> employeeOpt = employeeRepository.findById(requestDTO.getEmployeeId());
+        Optional<Employee> evaluatorOpt = employeeRepository.findById(requestDTO.getEvaluatorId());
+        Optional<EvaluationCriteria> criteriaOpt = evaluationCriteriaRepository.findById(requestDTO.getEvaluationTemplateId());
 
         if (employeeOpt.isEmpty() || evaluatorOpt.isEmpty()) {
             throw new IllegalArgumentException("Invalid employee or evaluator ID");
@@ -44,17 +54,22 @@ public class EvaluationServiceImpl implements EvaluationService {
 
         Employee employee = employeeOpt.get();
         Employee evaluator = evaluatorOpt.get();
+        EvaluationCriteria criteria = criteriaOpt.get();
 
         // 평가 생성
         Evaluation evaluation = Evaluation.builder()
                 .employee(employee)
                 .evaluator(evaluator)
+                .evaluationTemplate(criteria)
                 .score(requestDTO.getScore())
                 .comments(requestDTO.getComments())
                 .build();
 
+
         return evaluationRepository.save(evaluation);
     }
+
+
 
     @Override
     @Transactional
@@ -76,6 +91,55 @@ public class EvaluationServiceImpl implements EvaluationService {
         // 평가가 존재하는지 확인
         if (evaluationRepository.existsById(evaluationId)) {
             evaluationRepository.deleteById(evaluationId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public List<EvaluationCriteria> getEvaluationCriteriaList() {
+        return evaluationCriteriaRepository.findAll();
+    }
+
+    @Override
+    public EvaluationCriteria getEvaluationCriteriaById(Long criteriaId) {
+        return evaluationCriteriaRepository.findById(criteriaId)
+                .orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public EvaluationCriteria createEvaluationCriteria(EvaluationCriteriaReqDTO requestDTO) {
+        Department department = departmentRepository.findById(requestDTO.getDepartmentId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid department ID"));
+
+        EvaluationCriteria evaluationCriteria = EvaluationCriteria.fromRequestDTO(requestDTO, department);
+
+        return evaluationCriteriaRepository.save(evaluationCriteria);
+    }
+
+    @Override
+    @Transactional
+    public EvaluationCriteria updateEvaluationCriteria(Long criteriaId, EvaluationCriteriaReqDTO requestDTO) {
+        EvaluationCriteria criteria = evaluationCriteriaRepository.findById(criteriaId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 평가 기준 ID : " + criteriaId));
+
+        Department department = departmentRepository.findById(requestDTO.getDepartmentId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid department ID"));
+
+        // 평가 기준 정보 업데이트
+        criteria.setDepartment(department);
+        criteria.setCriteriaTitle(requestDTO.getCriteriaTitle());
+        criteria.setCriteriaContent(requestDTO.getCriteriaContent());
+
+        return evaluationCriteriaRepository.save(criteria);
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteEvaluationCriteria(Long criteriaId) {
+        if (evaluationCriteriaRepository.existsById(criteriaId)) {
+            evaluationCriteriaRepository.deleteById(criteriaId);
             return true;
         }
         return false;
