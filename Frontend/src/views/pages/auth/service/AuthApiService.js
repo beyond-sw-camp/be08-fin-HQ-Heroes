f220d107e334cfb0fa1e83f38a4e59d0a8a00e4a
jongwon <growjong8802@gmail.com>
2024-10-10 00:59:51 +0900

    feat : 인증된 사용자의 post, put, delete 요청 구현

--- Frontend/src/views/pages/auth/service/AuthApiService.js
+++ Frontend/src/views/pages/auth/service/AuthApiService.js
@@ -33,5 +33,94 @@ const fetchGet = async (url) => {
   return null;
 };
 
+const fetchPost = async (url, data) => {
+  const authStore = useAuthStore();
+  const router = useRouter();
+  const route = useRoute();
 
-export default fetchGet;
+  try {
+    const response = await axios.post(url, data, {
+      withCredentials: true,
+      headers: {
+        'Authorization': `Bearer ${authStore.accessToken}`,
+      }
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      const reissueSuccess = await fetchReissue();
+      if (reissueSuccess) {
+        return await fetchPost(url, data);
+      } else {
+        alert('관리자가 아닙니다.');
+        router.push({ path: '/login', query: { redirect: route.path } });
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching authorized page:', error);
+  }
+  return null;
+};
+
+const fetchPut = async (url, data) => {
+  const authStore = useAuthStore();
+  const router = useRouter();
+  const route = useRoute();
+
+  try {
+    const response = await axios.put(url, data, {
+      withCredentials: true,
+      headers: {
+        'Authorization': `Bearer ${authStore.accessToken}`,
+      }
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      const reissueSuccess = await fetchReissue();
+      if (reissueSuccess) {
+        return await fetchPut(url, data);
+      } else {
+        alert('관리자가 아닙니다.');
+        router.push({ path: '/login', query: { redirect: route.path } });
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching authorized page:', error);
+  }
+  return null;
+};
+
+const fetchDelete = async (url) => {
+  const authStore = useAuthStore();
+  const router = useRouter();
+  const route = useRoute();
+
+  try {
+    const response = await axios.delete(url, {
+      withCredentials: true,
+      headers: {
+        'Authorization': `Bearer ${authStore.accessToken}`,
+      }
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      const reissueSuccess = await fetchReissue();
+      if (reissueSuccess) {
+        return await fetchDelete(url);
+      } else {
+        alert('관리자가 아닙니다.');
+        router.push({ path: '/login', query: { redirect: route.path } });
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching authorized page:', error);
+  }
+  return null;
+};
+
+export { fetchGet, fetchPost, fetchPut, fetchDelete };
