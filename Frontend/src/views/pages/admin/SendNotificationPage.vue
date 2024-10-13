aee9b6def93c06fc5d5dc1f3735071921f7549de
jongwon <growjong8802@gmail.com>
2024-10-14 00:57:41 +0900

    design : 알림 작성 페이지 디자인 수정

--- Frontend/src/views/pages/admin/SendNotificationPage.vue
+++ Frontend/src/views/pages/admin/SendNotificationPage.vue
@@ -1,56 +1,60 @@
 <template>
-    <div class="app-container">
-        <div class="sidebar">
-            <!-- 검색 필드 -->
-            <div class="search-container">
-                <i class="pi pi-search search-icon"></i>
-                <input type="text" placeholder="Search employees..." v-model="searchQuery" class="search-input" />
-            </div>
+  <div class="app-container">
+    <div class="sidebar">
+      <!-- 검색 필드 -->
+      <div class="search-container">
+        <i class="pi pi-search search-icon"></i>
+        <input type="text" placeholder="Search employees..." v-model="searchQuery" class="search-input" />
+      </div>
 
-            <!-- 사원 목록 트리 구조 -->
-            <div class="panelmenu-container">
-                <div class="card">
-                    <div class="font-semibold text-xl">사원 목록</div>
-                    <Tree v-model:selectionKeys="selectedKeys" :value="filteredTreeData" selectionMode="checkbox" class="w-full md:w-[20rem]">
-                        <template #default="slotProps">
-                            <div class="flex items-center">
-                                <Avatar v-if="slotProps.node.key.startsWith('emp-') && !slotProps.node.profileImageUrl" label="X" size="normal" shape="circle" class="mr-2" style="background-color: #dee9fc; color: #1a2551" />
-                                <Avatar v-else-if="slotProps.node.key.startsWith('emp-')" :image="slotProps.node.profileImageUrl" size="normal" shape="circle" class="mr-2" />
-                                <span>{{ slotProps.node.label }}</span>
-                            </div>
-                        </template>
-                    </Tree>
-                </div>
-            </div>
+      <!-- 사원 목록 트리 구조 -->
+      <div class="panelmenu-container">
+        <div class="card">
+          <div class="font-semibold text-xl">사원 목록</div>
+          <Tree v-model:selectionKeys="selectedKeys" :value="filteredTreeData" selectionMode="checkbox"
+            class="w-full md:w-[20rem]">
+            <template #default="slotProps">
+              <div class="flex items-center">
+                <Avatar v-if="slotProps.node.key.startsWith('emp-') && !slotProps.node.profileImageUrl" label="X"
+                  size="normal" shape="circle" class="mr-2" style="background-color: #dee9fc; color: #1a2551" />
+                <Avatar v-else-if="slotProps.node.key.startsWith('emp-')" :image="slotProps.node.profileImageUrl"
+                  size="normal" shape="circle" class="mr-2" />
+                <span>{{ slotProps.node.label }}</span>
+              </div>
+            </template>
+          </Tree>
         </div>
-
-        <!-- 메시지 작성 영역 -->
-        <div class="notification-container">
-            <div class="compose-message-container">
-                <div class="compose-message">
-                    <h3 class="mb-2"><b>발송자</b></h3>
-                    <InputText type="text" v-model="sender" class="to-input" />
-
-                    <!-- Notification Category Dropdown -->
-                    <h3 class="mb-2 mt-5"><b>카테고리</b></h3>
-                    <Select v-model="selectedCategory" :options="categories" optionLabel="categoryName" placeholder="카테고리를 선택하세요" />
-
-                    <h3 class="mb-2 mt-5"><b>메시지</b></h3>
-                    <div ref="editor" class="message-editor"></div>
-
-                    <div class="button-container">
-                        <button @click="sendMessage" class="send-button">알림 발송</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <!-- 알림 발송 진행상황 모달 -->
-        <Dialog header="알림 발송 중" v-model:visible="progressVisible" :modal="true" :closable="false" style="width: 50vw">
-            <ProgressBar :value="progressValue"></ProgressBar>
-            <p>{{ progressValue }}% 완료</p>
-        </Dialog>
+      </div>
     </div>
+
+    <!-- 메시지 작성 영역 -->
+    <div class="notification-container">
+      <div class="compose-message-container">
+        <div class="compose-message">
+          <h3 class="mb-2"><b>발송자</b></h3>
+          <InputText type="text" v-model="sender" class="to-input" />
+
+          <!-- Notification Category Dropdown -->
+          <h3 class="mb-2 mt-5"><b>카테고리</b></h3>
+          <Select v-model="selectedCategory" :options="categories" optionLabel="categoryName"
+            placeholder="카테고리를 선택하세요" />
+
+          <h3 class="mb-2 mt-5"><b>메시지</b></h3>
+          <div ref="editor" class="message-editor"></div>
+
+          <div class="button-container">
+            <button @click="sendMessage" class="send-button">알림 발송</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- 알림 발송 진행상황 모달 -->
+    <Dialog header="알림 발송 중" v-model:visible="progressVisible" :modal="true" :closable="false" style="width: 50vw">
+      <ProgressBar :value="progressValue"></ProgressBar>
+      <p>{{ progressValue }}% 완료</p>
+    </Dialog>
+  </div>
 </template>
 
 <script setup>
@@ -80,264 +84,264 @@ const progressValue = ref(0); // ProgressBar의 현재 값
 const progressVisible = ref(false); // ProgressBar 모달 표시 여부
 
 onMounted(() => {
-    fetchEmployeeList();
-    fetchCategories(); // Fetch notification categories
+  fetchEmployeeList();
+  fetchCategories(); // Fetch notification categories
 
-    const quillEditor = new Quill(editor.value, {
-        theme: 'snow',
-        modules: {
-            toolbar: [[{ font: [] }, { size: [] }], ['bold', 'italic', 'underline', 'strike'], [{ color: [] }, { background: [] }], [{ list: 'ordered' }, { list: 'bullet' }], [{ align: [] }], ['link', 'image', 'blockquote'], ['clean']]
-        }
-    });
+  const quillEditor = new Quill(editor.value, {
+    theme: 'snow',
+    modules: {
+      toolbar: [[{ font: [] }, { size: [] }], ['bold', 'italic', 'underline', 'strike'], [{ color: [] }, { background: [] }], [{ list: 'ordered' }, { list: 'bullet' }], [{ align: [] }], ['link', 'image', 'blockquote'], ['clean']]
+    }
+  });
 
-    quillEditor.on('text-change', () => {
-        message.value = quillEditor.root.innerHTML;
-    });
+  quillEditor.on('text-change', () => {
+    message.value = quillEditor.root.innerHTML;
+  });
 });
 
 async function fetchEmployeeList() {
-    try {
-        const data = await fetchGet('http://localhost:8080/api/v1/employee/employees');
-        employeeData.value = convertToTreeModel(data);
-    } catch (error) {
-        console.error('Error fetching employee data:', error);
-    }
+  try {
+    const data = await fetchGet('http://localhost:8080/api/v1/employee/employees');
+    employeeData.value = convertToTreeModel(data);
+  } catch (error) {
+    console.error('Error fetching employee data:', error);
+  }
 }
 
 async function fetchCategories() {
-    try {
-        const response = await fetchGet('http://localhost:8080/api/v1/notification-service/categories');
-        categories.value = response;
-    } catch (error) {
-        console.error('Error fetching categories:', error);
-    }
+  try {
+    const response = await fetchGet('http://localhost:8080/api/v1/notification-service/categories');
+    categories.value = response;
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+  }
 }
 
 const convertToTreeModel = (data) => {
-    const departments = data.reduce((acc, employee) => {
-        const dept = acc.find((d) => d.label === employee.deptName);
-        if (!dept) {
-            acc.push({
-                key: `dept-${employee.deptName}`,
-                label: employee.deptName,
-                icon: 'pi pi-building',
-                children: [
-                    {
-                        key: `team-${employee.teamName}`,
-                        label: employee.teamName,
-                        icon: 'pi pi-users',
-                        children: []
-                    }
-                ]
-            });
-        }
-        const deptIndex = acc.findIndex((d) => d.label === employee.deptName);
-        const team = acc[deptIndex].children.find((t) => t.label === employee.teamName);
-        if (!team) {
-            acc[deptIndex].children.push({
-                key: `team-${employee.teamName}`,
-                label: employee.teamName,
-                icon: 'pi pi-users',
-                children: []
-            });
-        }
-        const teamIndex = acc[deptIndex].children.findIndex((t) => t.label === employee.teamName);
-        acc[deptIndex].children[teamIndex].children.push({
-            key: `emp-${employee.employeeId}`,
-            label: employee.employeeName,
-            profileImageUrl: employee.profileImageUrl,
-            employeeId: employee.employeeId,
-            jobName: employee.jobName,
-            positionName: employee.positionName,
-            joinDate: employee.joinDate
-        });
-        return acc;
-    }, []);
-    return departments;
+  const departments = data.reduce((acc, employee) => {
+    const dept = acc.find((d) => d.label === employee.deptName);
+    if (!dept) {
+      acc.push({
+        key: `dept-${employee.deptName}`,
+        label: employee.deptName,
+        icon: 'pi pi-building',
+        children: [
+          {
+            key: `team-${employee.teamName}`,
+            label: employee.teamName,
+            icon: 'pi pi-users',
+            children: []
+          }
+        ]
+      });
+    }
+    const deptIndex = acc.findIndex((d) => d.label === employee.deptName);
+    const team = acc[deptIndex].children.find((t) => t.label === employee.teamName);
+    if (!team) {
+      acc[deptIndex].children.push({
+        key: `team-${employee.teamName}`,
+        label: employee.teamName,
+        icon: 'pi pi-users',
+        children: []
+      });
+    }
+    const teamIndex = acc[deptIndex].children.findIndex((t) => t.label === employee.teamName);
+    acc[deptIndex].children[teamIndex].children.push({
+      key: `emp-${employee.employeeId}`,
+      label: employee.employeeName,
+      profileImageUrl: employee.profileImageUrl,
+      employeeId: employee.employeeId,
+      jobName: employee.jobName,
+      positionName: employee.positionName,
+      joinDate: employee.joinDate
+    });
+    return acc;
+  }, []);
+  return departments;
 };
 
 // 검색 필터링
 const filteredTreeData = computed(() => {
-    if (!searchQuery.value) return employeeData.value;
+  if (!searchQuery.value) return employeeData.value;
 
-    const filterTree = (items, query) => {
-        return items
-            .map((item) => {
-                if (item.children) {
-                    const filteredChildren = filterTree(item.children, query);
-                    if (filteredChildren.length || item.label.toLowerCase().includes(query)) {
-                        return { ...item, children: filteredChildren };
-                    }
-                } else if (item.label.toLowerCase().includes(query)) {
-                    return item;
-                }
-                return null;
-            })
-            .filter((item) => item !== null);
-    };
+  const filterTree = (items, query) => {
+    return items
+      .map((item) => {
+        if (item.children) {
+          const filteredChildren = filterTree(item.children, query);
+          if (filteredChildren.length || item.label.toLowerCase().includes(query)) {
+            return { ...item, children: filteredChildren };
+          }
+        } else if (item.label.toLowerCase().includes(query)) {
+          return item;
+        }
+        return null;
+      })
+      .filter((item) => item !== null);
+  };
 
-    return filterTree(employeeData.value, searchQuery.value.toLowerCase());
+  return filterTree(employeeData.value, searchQuery.value.toLowerCase());
 });
 
 // 단일 알림 발송
 const sendNotification = async (notificationPayload) => {
-    try {
-        const response = await fetchPost('http://localhost:8080/api/v1/notification-service/notification', notificationPayload);
-        if (response) {
-            console.log('Notification sent successfully:', response);
-        } else {
-            console.error('Failed to send notification.');
-        }
-    } catch (error) {
-        console.error('Error sending notification:', error);
+  try {
+    const response = await fetchPost('http://localhost:8080/api/v1/notification-service/notification', notificationPayload);
+    if (response) {
+      console.log('Notification sent successfully:', response);
+    } else {
+      console.error('Failed to send notification.');
     }
+  } catch (error) {
+    console.error('Error sending notification:', error);
+  }
 };
 
 // 알림 발송 시 진행률 표시
 const sendMessage = async () => {
-    const selectedEmployeeIds = Object.keys(selectedKeys.value)
-        .filter((key) => key.startsWith('emp-'))
-        .map((key) => key.replace('emp-', ''));
+  const selectedEmployeeIds = Object.keys(selectedKeys.value)
+    .filter((key) => key.startsWith('emp-'))
+    .map((key) => key.replace('emp-', ''));
 
-    if (!selectedCategory.value) {
-        alert('카테고리를 선택하세요.');
-        return;
-    }
+  if (!selectedCategory.value) {
+    alert('카테고리를 선택하세요.');
+    return;
+  }
 
-    if (selectedEmployeeIds.length === 0) {
-        alert('직원을 선택하세요.');
-        return;
-    }
+  if (selectedEmployeeIds.length === 0) {
+    alert('직원을 선택하세요.');
+    return;
+  }
 
-    const senderId = window.localStorage.getItem('employeeId');
-    progressVisible.value = true; // ProgressBar 모달을 표시
-    progressValue.value = 0; // ProgressBar 초기화
+  const senderId = window.localStorage.getItem('employeeId');
+  progressVisible.value = true; // ProgressBar 모달을 표시
+  progressValue.value = 0; // ProgressBar 초기화
 
-    for (let i = 0; i < selectedEmployeeIds.length; i++) {
-        const receiverId = selectedEmployeeIds[i];
-        const payload = {
-            senderId,
-            receiverId,
-            categoryId: selectedCategory.value.notificationCategoryId,
-            message: message.value,
-            status: 'UNREAD' // 기본 상태 설정
-        };
+  for (let i = 0; i < selectedEmployeeIds.length; i++) {
+    const receiverId = selectedEmployeeIds[i];
+    const payload = {
+      senderId,
+      receiverId,
+      categoryId: selectedCategory.value.notificationCategoryId,
+      message: message.value,
+      status: 'UNREAD' // 기본 상태 설정
+    };
 
-        await sendNotification(payload);
-        // ProgressBar 값 업데이트
-        progressValue.value = Math.round(((i + 1) / selectedEmployeeIds.length) * 100);
-    }
+    await sendNotification(payload);
+    // ProgressBar 값 업데이트
+    progressValue.value = Math.round(((i + 1) / selectedEmployeeIds.length) * 100);
+  }
 
-    progressVisible.value = false; // ProgressBar 모달 닫기;
+  progressVisible.value = false; // ProgressBar 모달 닫기;
 
-    setTimeout(() => {
-        alert('알림 발송 완료');
-    }, 100);
+  setTimeout(() => {
+    alert('알림 발송 완료');
+  }, 100);
 };
 </script>
 
 <style scoped>
-
 .app-container {
-    display: flex;
-    height: 50rem;
+  display: flex;
+  height: 50rem;
 }
 
 .sidebar {
-    background-color: #ffffff;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    overflow-y: auto;
-    display: flex;
-    flex-direction: column;
+  background-color: #ffffff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
 }
 
 .panelmenu-container {
-    flex-grow: 1;
+  flex-grow: 1;
 }
 
 .card {
-    padding: 0;
-    text-align: left;
+  padding: 0;
+  text-align: left;
 }
 
 .font-semibold.text-xl {
-    padding: 10px;
+  padding: 10px;
 }
 
 .search-container {
-    position: relative;
+  position: relative;
 }
 
 .search-icon {
-    position: absolute;
-    left: 10px;
-    top: 50%;
-    transform: translateY(-50%);
-    color: #aaa;
+  position: absolute;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  color: #aaa;
 }
 
 .search-input {
-    padding: 10px 10px 10px 40px;
-    margin-bottom: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    width: 100%;
+  padding: 10px 10px 10px 40px;
+  margin-bottom: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  width: 100%;
 }
 
 .notification-container {
-    width: 75%;
-    padding-left: 20px;
-    display: flex;
-    flex-direction: column;
+  width: 75%;
+  padding-left: 20px;
+  display: flex;
+  flex-direction: column;
 }
 
 .compose-message-container {
-    flex-grow: 1;
-    background-color: #ffffff;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    padding: 20px;
-    display: flex;
-    flex-direction: column;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  flex-grow: 1;
+  background-color: #ffffff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
 .to-input {
-    padding: 10px;
-    font-size: 16px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-    width: 20%;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  width: 20%;
 }
 
 .message-editor {
-    flex-grow: 1;
-    font-size: 16px;
-    overflow-y: auto;
-    border: 1px solid #ddd;
-    border-radius: 5px;
+  flex-grow: 1;
+  font-size: 16px;
+  height: 25rem;
+  overflow-y: auto;
+  border: 1px solid #ddd;
+  border-radius: 5px;
 }
 
 .button-container {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 
 .send-button {
-    padding: 10px 20px;
-    margin-top: 10px;
-    background-color: #6366f1;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-    transition: background-color 0.2s;
+  padding: 10px 20px;
+  margin-top: 10px;
+  background-color: #6366f1;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.2s;
 }
 
 .send-button:hover {
-    background-color: #4f46e5;
+  background-color: #4f46e5;
 }
 </style>
