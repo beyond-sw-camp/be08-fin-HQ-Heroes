df090b04f56c0d9ea49d6ed0d71656148a205189
eunhee78 <dmsgml4579@naver.com>
2024-10-15 21:41:51 +0900

    feat : 공제액 조회 구현

--- Frontend/src/views/pages/salary/deductService.js
+++ Frontend/src/views/pages/salary/deductService.js
@@ -5,7 +5,7 @@ const API_URL = 'http://localhost:8080/api/v1/deduct-service/deductions';
 // 모든 공제 항목 가져오기
 export const fetchDeductions = async () => {
     try {
-      const response = await axios.get(`${API_URL}`);
+      const response = await axios.get(`${API_URL}`); 
       return response.data;
     } catch (error) {
       console.error('Error fetching deductions:', error);
--- Frontend/src/views/pages/salary/salaryService.js
+++ Frontend/src/views/pages/salary/salaryService.js
@@ -2,28 +2,28 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/v1/salary-service';
 
-// 현재 년도까지 데이터 가져오기
-export const fetchSalaryDataUpToCurrentMonth = async (employeeId, year) => {
+// 연도별 급여 데이터 가져오기
+export const fetchSalariesByEmployeeIdAndYear = async (employeeId, year) => {
   try {
     const response = await axios.get(`${API_URL}/histories`, {
-      params: { employeeId, year }
+      params: { employeeId, year },
     });
-    return response.data; // 각 월에 대한 데이터 반환
+    return response.data;
   } catch (error) {
-    console.error('Error fetching salary data up to current month:', error);
-    throw error; // 에러가 발생하면 다시 던지기
+    console.error('Error fetching salary data by year:', error);
+    throw error;
   }
 };
 
-// 특정 월의 급여 데이터 가져오기
-export const fetchSalaryDetailsForMonth = async (employeeId, year, month) => {
+// 해당 년도, 해당 월의 급여 데이터 가져오기
+export const fetchSalaryByEmployeeIdAndMonth = async (employeeId, year, month) => {
   try {
-    const response = await axios.get(`${API_URL}/histories/month`, { // 엔드포인트 수정 필요할 수 있음
-      params: { employeeId, year, month }
+    const response = await axios.get(`${API_URL}/histories/month`, {
+      params: { employeeId, year, month },
     });
-    return response.data; // 선택한 월의 급여 데이터 반환
+    return response.data;
   } catch (error) {
-    console.error('Error fetching salary details for month:', error);
-    throw error; // 에러가 발생하면 다시 던지기
+    console.error('Error fetching salary data by month:', error);
+    throw error;
   }
 };
