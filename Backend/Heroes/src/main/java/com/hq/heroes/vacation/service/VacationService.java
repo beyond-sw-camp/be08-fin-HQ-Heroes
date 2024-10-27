dbe36e4250ca8f2ff8910ef9cbce85c8e5ced768
jongwon <growjong8802@gmail.com>
2024-10-28 01:20:56 +0900

    feat : 휴가 신청, 승인, 반려 알림 발송 구현

--- Backend/Heroes/src/main/java/com/hq/heroes/vacation/service/VacationService.java
+++ Backend/Heroes/src/main/java/com/hq/heroes/vacation/service/VacationService.java
@@ -1,12 +1,13 @@
 package com.hq.heroes.vacation.service;
 
 import com.hq.heroes.vacation.dto.VacationDTO;
+import com.hq.heroes.vacation.entity.Vacation;
 
 import java.util.List;
 
 public interface VacationService {
 
-    void submitVacation(VacationDTO vacationDTO);
+    Vacation submitVacation(VacationDTO vacationDTO);
 
     List<VacationDTO> getAllVacations();
 
